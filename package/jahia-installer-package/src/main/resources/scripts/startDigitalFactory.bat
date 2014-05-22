e3fea05b4f3f606d5401a2fa6742e49387aaa60f
Sergiy Shyrkov <sshyrkov@jahia.com>
2014-05-22 15:25:28 +0000

    QA-5927 - Windows: startJahia fails with no explanation if environment not properly set up https://jira.jahia.org/browse/QA-5927
    In the Windows startup script check for presence of the JAVA_HOME variable and keep the window open in case it is not set to see the error message
    
    git-svn-id: https://devtools.jahia.com/svn/jahia_pack/trunk@965 39555585-ee67-472c-bf1e-947fb1f6b7c6

--- package/jahia-installer-package/src/main/resources/scripts/startDigitalFactory.bat
+++ package/jahia-installer-package/src/main/resources/scripts/startDigitalFactory.bat
@@ -4,6 +4,29 @@ echo ---------------------------------------------------------------------------
 echo Starting Digital Factory Server
 echo ---------------------------------------------------------------------------
 
+if "x%JAVA_HOME%" == "x" (
+    echo JAVA_HOME environment variable is not defined. It is required for running the Digital Factory Server.
+	echo Please set JAVA_HOME environment variable to point to the directory with the your Java SE Development Kit.
+	goto exit
+) else (
+  if not exist "%JAVA_HOME%" (
+    echo JAVA_HOME "%JAVA_HOME%" path doesn't exist
+	echo Please set JAVA_HOME environment variable to point to the directory with the your Java SE Development Kit.
+    goto exit
+  )
+)
+
 cd /d %~dp0\tomcat\bin
+
 call startup.bat
+
+goto end
+
+:exit
+if "x%DF_SOURCE_INSTALLER%" == "x" (
+    pause
+)
+exit /b 1
+
+:end
 exit /b 0
