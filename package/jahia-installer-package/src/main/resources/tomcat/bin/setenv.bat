cec2a3b27fe4f0cc21da79f2b7f661ccb89b167d
bamboo <bamboo@jahia.com>
2015-04-10 15:54:11 +0000

    QA-7335 - set -Xms2g -Xmx2g -XX:MaxPermSize=384m JVM memory options by default and do not check if JDK is 32bit or not, as DF is not starting with less than 1960m. (commit by sshyrkov merged from jahia_pack/branches/JAHIA-7-0-X-X-BRANCH revision 1310)
    git-svn-id: https://devtools.jahia.com/svn/jahia_pack/trunk@1311 39555585-ee67-472c-bf1e-947fb1f6b7c6

--- package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.bat
+++ package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.bat
@@ -2,5 +2,5 @@ rem ---------------------------------------------------------------------------
 rem Digital Factory settings
 rem ---------------------------------------------------------------------------
 
-set CATALINA_OPTS=%CATALINA_OPTS% -Dsun.io.useCanonCaches=false -Xms1152m -Xmx1152m -XX:MaxPermSize=256m -server -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true
+set CATALINA_OPTS=%CATALINA_OPTS% -Dsun.io.useCanonCaches=false -Xms2g -Xmx2g -XX:MaxPermSize=384m -server -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true
 set CATALINA_OPTS=%CATALINA_OPTS% -Dderby.system.home="%{derby.home.win}"
--- package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
+++ package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
@@ -4,7 +4,7 @@
 # Digital Factory settings
 # ---------------------------------------------------------------------------
 
-CATALINA_OPTS="$CATALINA_OPTS -server -Xms1152m -Xmx1152m -XX:MaxPermSize=256m -Djava.awt.headless=true -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true"
+CATALINA_OPTS="$CATALINA_OPTS -server -Xms2g -Xmx2g -XX:MaxPermSize=384m -Djava.awt.headless=true -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true"
 CATALINA_OPTS="$CATALINA_OPTS -Dderby.system.home=%{derby.home.unix}"
 export CATALINA_OPTS
 export CATALINA_PID=$CATALINA_HOME/temp/tomcat.pid
