de649e1306b62f9efb910743c335892760a6a401
bamboo <bamboo@jahia.com>
2015-04-15 11:01:42 +0000

    QA-7335 - set -Xms2048m -Xmx2048m (commit by sshyrkov merged from jahia_pack/branches/JAHIA-7-0-X-X-BRANCH revision 1315)
    git-svn-id: https://devtools.jahia.com/svn/jahia_pack/trunk@1316 39555585-ee67-472c-bf1e-947fb1f6b7c6

--- package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
+++ package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
@@ -4,7 +4,7 @@
 # Digital Factory settings
 # ---------------------------------------------------------------------------
 
-CATALINA_OPTS="$CATALINA_OPTS -server -Xms2g -Xmx2g -XX:MaxPermSize=384m -Djava.awt.headless=true -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true"
+CATALINA_OPTS="$CATALINA_OPTS -server -Xms2048m -Xmx2048m -XX:MaxPermSize=384m -Djava.awt.headless=true -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true"
 CATALINA_OPTS="$CATALINA_OPTS -Dderby.system.home=%{derby.home.unix}"
 export CATALINA_OPTS
 export CATALINA_PID=$CATALINA_HOME/temp/tomcat.pid
