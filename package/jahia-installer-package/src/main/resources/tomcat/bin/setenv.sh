441d0a4e05309c91ccebd3f86b993d764cd39824
bamboo <bamboo@jahia.com>
2015-12-10 10:35:50 +0000

    BACKLOG-6092 - adjusted labels in setenv.* script files (commit by sshyrkov merged from jahia_pack/branches/JAHIA-7-1-X-X-BRANCH revision 1535)
    git-svn-id: https://devtools.jahia.com/svn/jahia_pack/trunk@1536 39555585-ee67-472c-bf1e-947fb1f6b7c6

--- package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
+++ package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ---------------------------------------------------------------------------
-# Digital Factory settings
+# Digital Experience Manager settings
 # ---------------------------------------------------------------------------
 
 CATALINA_OPTS="$CATALINA_OPTS -server -Xms2048m -Xmx2048m -XX:MaxPermSize=384m -Djava.awt.headless=true -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true"
