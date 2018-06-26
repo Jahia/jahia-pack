e05e087565104fc4a29c231fb5a323affb452a76
Sergiy Shyrkov <sshyrkov@jahia.com>
2018-06-26 18:17:35 +0200

    [QA-10785] Dropped -XX:MaxPermSize JVM option which is no longer supported in JDK 8

--- package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.bat
+++ package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.bat
@@ -2,5 +2,5 @@ rem ---------------------------------------------------------------------------
 rem Digital Experience Manager settings
 rem ---------------------------------------------------------------------------
 
-set CATALINA_OPTS=%CATALINA_OPTS% -Dsun.io.useCanonCaches=false -Xms2048m -Xmx2048m -XX:MaxPermSize=384m -server -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true
+set CATALINA_OPTS=%CATALINA_OPTS% -Dsun.io.useCanonCaches=false -Xms2048m -Xmx2048m -server -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true
 set CATALINA_OPTS=%CATALINA_OPTS% -Dderby.system.home="%{derby.home.win}"
