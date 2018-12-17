4a76d5b6ce3d3e87f2fe897b627e11afb5ef29c5
Sergiy Shyrkov <sshyrkov@jahia.com>
2018-12-17 23:08:59 +0100

    [BACKLOG-9218] If an environment variable JAHIA_JAVA_OPTS is set, we assume that is contains the JVM heap memory options (-Xms, -Xmx) to be used. Otherwise, we use the defaults.

--- package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
+++ package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
@@ -4,7 +4,12 @@
 # Digital Experience Manager settings
 # ---------------------------------------------------------------------------
 
-CATALINA_OPTS="$CATALINA_OPTS -server -Xms2048m -Xmx2048m -Djava.awt.headless=true -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true"
+if [ -z "$JAHIA_JAVA_OPTS" ] ; then
+    JAHIA_JAVA_OPTS="-Xms2048m -Xmx2048m"
+    echo "JAHIA_JAVA_OPTS environment variable is not set. Using default options: $JAHIA_JAVA_OPTS"
+fi
+
+CATALINA_OPTS="$CATALINA_OPTS -server -Djava.awt.headless=true -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true $JAHIA_JAVA_OPTS"
 CATALINA_OPTS="$CATALINA_OPTS -Dderby.system.home=%{derby.home.unix}"
 export CATALINA_OPTS
 export CATALINA_PID=$CATALINA_HOME/temp/tomcat.pid
