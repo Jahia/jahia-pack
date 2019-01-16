4daeec51a8e39083222479673e08011cfe7d7cec
Serge Huber <shuber@jahia.com>
2019-01-16 14:32:15 +0100

    QA-10454 SIGTERM incorrectly handled by Karaf

--- package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
+++ package/jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
@@ -6,5 +6,6 @@
 
 CATALINA_OPTS="$CATALINA_OPTS -server -Xms2048m -Xmx2048m -Djava.awt.headless=true -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true"
 CATALINA_OPTS="$CATALINA_OPTS -Dderby.system.home=%{derby.home.unix}"
+CATALINA_OPTS="$CATALINA_OPTS -Dkaraf.handle.sigterm=false"
 export CATALINA_OPTS
 export CATALINA_PID=$CATALINA_HOME/temp/tomcat.pid
