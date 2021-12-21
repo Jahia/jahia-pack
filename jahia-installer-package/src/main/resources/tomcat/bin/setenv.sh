c986e69eb16bc8fe91d279bcc1e9253995fd9df6
Michael De Checchi <mdechecchi@jahia.com>
2021-12-21 10:54:13 +0100

    BACKLOG-16660 - Add formatMsgNoLookups option (#363)

--- jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
+++ jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
@@ -21,5 +21,6 @@ echo "Using JVM options: -Xms$JAHIA_JAVA_XMS -Xmx$JAHIA_JAVA_XMX $JAHIA_JAVA_OPT
 CATALINA_OPTS="$CATALINA_OPTS -server -Xms$JAHIA_JAVA_XMS -Xmx$JAHIA_JAVA_XMX -Djava.awt.headless=true -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintConcurrentLocks -Djava.net.preferIPv4Stack=true -Djavax.el.class-resolution.disableOnLowerCase=true $JAHIA_JAVA_OPTS"
 CATALINA_OPTS="$CATALINA_OPTS -Dderby.system.home=%{derby.home.unix}"
 CATALINA_OPTS="$CATALINA_OPTS -Dkaraf.handle.sigterm=false"
+CATALINA_OPTS="$CATALINA_OPTS -Dlog4j2.formatMsgNoLookups=true"
 export CATALINA_OPTS
 export CATALINA_PID=$CATALINA_HOME/temp/tomcat.pid
