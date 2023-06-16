5a3888669a2f157df06d0369bcc93906f70de7f6
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2023-06-16 10:35:02 -0400

    TECH-1321: Add support for target param in bundle scripts (#583)
    * TECH-1321: Add support for target param in bundle scripts
    
    * Add usage details for installBundle.sh
    
    * missing new line char

--- jahia-installer-package/src/main/resources/tools/installBundle.sh
+++ jahia-installer-package/src/main/resources/tools/installBundle.sh
@@ -3,11 +3,12 @@
 
 DX_DISPLAY_USAGE="Script to install a bundle using the Module management REST API.\n
 if the second parameter is set to true, the bundle is started\n
-\nUsage:\n$0 bundleFilePath  [ true ]\n"
+optional third parameter to specify cluster node group, or local to deploy to a node individually. Installs to default cluster node group if left blank\n
+\nUsage:\n$0 bundleFilePath  [ true ] [ target ]\n"
 
 source ./setupBundleApi.sh
 
 #deploy bundle REST call
 echo Installing bundle file $1...
-INSTALL_OUTPUT=`curl $CURL_OPTIONS --form bundle=@$1 --form start=$2 $DX_REST_URL/`
+INSTALL_OUTPUT=`curl $CURL_OPTIONS --form bundle=@$1 --form start=$2 --form target=$3 $DX_REST_URL/`
 echo "Install result=$INSTALL_OUTPUT"
--- jahia-installer-package/src/main/resources/tools/installBundles.sh
+++ jahia-installer-package/src/main/resources/tools/installBundles.sh
@@ -3,6 +3,7 @@
 
 DX_DISPLAY_USAGE="Script to install multiple bundles using the Module management REST API.\n
 if parameter '-s' or '--start' is specified, the bundles are started\n
+optional parameter '--target=<target-name>' to specify cluster node group, or local to deploy to a node individually\n
 \nUsage:\n$0 bundleFilePath [bundleFilePathZ ... bundleFilePathN] [ -s | --start ]\n"
 
 source ./setupBundleApi.sh
@@ -10,10 +11,13 @@ source ./setupBundleApi.sh
 bundlesParam=
 count=0
 start=false
+target=
 
 while [ "$1" != "" ]; do
     case $1 in
         -s | --start )          start=true
+                                ;;
+        --target=* )            target="${1#*=}"
                                 ;;
         * )                     bundlesParam="$bundlesParam --form bundle=@$1"
                                 count=$((count+1))
@@ -29,5 +33,5 @@ fi
 
 #deploy bundles REST call
 echo "Installing $count bundles files ..."
-INSTALL_OUTPUT=`curl $CURL_OPTIONS $bundlesParam --form start=$start $DX_REST_URL/`
+INSTALL_OUTPUT=`curl $CURL_OPTIONS $bundlesParam --form start=$start --form target=$target $DX_REST_URL/`
 echo "Install result=$INSTALL_OUTPUT"
