e71a6e19254aaf9f823987140172b5dc3e819e5b
Cedric Mailleux <cmailleux@jahia.com>
2024-02-14 12:06:54 -0500

    BACKLOG-22382: Add XML XXE setup to use JDK embedded factories by default (#650)

--- jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
+++ jahia-installer-package/src/main/resources/tomcat/bin/setenv.sh
@@ -4,16 +4,16 @@
 # Jahia settings
 # ---------------------------------------------------------------------------
 
-if [ -z "$JAHIA_JAVA_XMS" ] ; then
-    JAHIA_JAVA_XMS="2048m"
+if [ -z "$JAHIA_JAVA_XMS" ]; then
+  JAHIA_JAVA_XMS="2048m"
 fi
 
-if [ -z "$JAHIA_JAVA_XMX" ] ; then
-    JAHIA_JAVA_XMX="2048m"
+if [ -z "$JAHIA_JAVA_XMX" ]; then
+  JAHIA_JAVA_XMX="2048m"
 fi
 
-if [ -z "$JAHIA_JAVA_OPTS" ] ; then
-    JAHIA_JAVA_OPTS=""
+if [ -z "$JAHIA_JAVA_OPTS" ]; then
+  JAHIA_JAVA_OPTS=""
 fi
 
 echo "Using JVM options: -Xms$JAHIA_JAVA_XMS -Xmx$JAHIA_JAVA_XMX $JAHIA_JAVA_OPTS"
@@ -22,6 +22,7 @@ CATALINA_OPTS="$CATALINA_OPTS -server -Xms$JAHIA_JAVA_XMS -Xmx$JAHIA_JAVA_XMX -D
 CATALINA_OPTS="$CATALINA_OPTS -Dderby.system.home=%{derby.home.unix}"
 CATALINA_OPTS="$CATALINA_OPTS -Dkaraf.handle.sigterm=false"
 CATALINA_OPTS="$CATALINA_OPTS -Dlog4j2.formatMsgNoLookups=true"
+CATALINA_OPTS="$CATALINA_OPTS -Djavax.xml.validation.SchemaFactory:http://www.w3.org/2001/XMLSchema=com.sun.org.apache.xerces.internal.jaxp.validation.XMLSchemaFactory -Djavax.xml.transform.TransformerFactory=com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl"
 export CATALINA_OPTS
 export CATALINA_PID=$CATALINA_HOME/temp/tomcat.pid
 export JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=java.base/java.net=ALL-UNNAMED  --add-exports org.graalvm.truffle/com.oracle.truffle.api.nodes=ALL-UNNAMED --add-exports org.graalvm.truffle/com.oracle.truffle.api.instrumentation=ALL-UNNAMED --add-exports org.graalvm.truffle/com.oracle.truffle.api.dsl=ALL-UNNAMED --add-exports org.graalvm.truffle/com.oracle.truffle.api=ALL-UNNAMED --add-exports org.graalvm.truffle/com.oracle.truffle.api.exception=ALL-UNNAMED --add-exports org.graalvm.truffle/com.oracle.truffle.api.frame=ALL-UNNAMED --add-exports org.graalvm.truffle/com.oracle.truffle.api.object=ALL-UNNAMED --add-exports org.graalvm.truffle/com.oracle.truffle.api.interop=ALL-UNNAMED --add-exports org.graalvm.truffle/com.oracle.truffle.api.strings=ALL-UNNAMED --add-exports org.graalvm.truffle/com.oracle.truffle.api.library=ALL-UNNAMED"
